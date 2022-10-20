ffd690529a2d779eea219ce961b2162f52bbe9f9
ham <xieych@deepblueai.com>
2022-10-20 10:20:59 +0800

    creat:最长有效括号

---
+++ Part 1/32_最长有效括号.py
@@ -0,0 +1,75 @@
+"""
+给你一个只包含 '('和 ')'的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+
+示例 1：
+输入：s = "(()"
+输出：2
+解释：最长有效括号子串是 "()"
+
+示例 2：
+输入：s = ")()())"
+输出：4
+解释：最长有效括号子串是 "()()"
+
+示例 3：
+输入：s = ""
+输出：0
+"""
+class Solution:
+    def longestValidParentheses(self, s):
+        if not s:
+            return 0
+        res = 0
+        stack = [-1]
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    res = max(res, i-stack[-1])
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
